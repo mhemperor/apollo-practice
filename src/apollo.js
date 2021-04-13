300de544587f7853016086350c2750aa438c1790
hyunjin <shsan0324@gmail.com>
2021-04-13 22:09:16 +0900

    Apollo Client

---
+++ src/apollo.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache: cache,
+  uri: "http://localhost:4000/",
+});
+
+export default client;
